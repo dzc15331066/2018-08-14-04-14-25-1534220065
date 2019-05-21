2421002a9fbe2e318b4518c19d4f5bdd6ce1bc59
dengzhc <2814414634@qq.com>
2019-05-21 21:44:36 +0800

    update main/main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,22 @@
 module.exports = function main() {
     console.log("Debug Info");
-    return 'Hello World!';
+    var nums =["._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.",
+           "|.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|",
+           "|_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|"];
+    var ns = [];
+    for(var i = 0; i < 3; i++){
+        ns.push(nums[i].split("   "));
+    }
+    var input = arguments[0];
+    var res = [[],[],[]];
+    for(var i = 0; i < input.length; i++){
+        var n = parseInt(input[i]);
+        for(var j = 0; j < 3; j++){
+            res[j].push(ns[j][n]);
+        }
+    }
+    for(var i = 0; i < 3; i++){
+        res[i] = res[i].join(" ");
+    }
+    return res.join("\n")+"\n";
 };
